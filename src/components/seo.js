b6820243bd7af9fa556f0e4774942b2c4ed1dcec
yusuke <tigers14@ezweb.ne.jp>
2021-09-26 11:14:36 +0900

    SEO

---
+++ src/components/seo.js
@@ -0,0 +1,27 @@
+import { useRouter } from "next/dist/client/router";
+import { Helmet } from "react-helmet";
+
+const SEO = ({ title, description }) => {
+  const router = useRouter()
+  const baseUrl = "http://localhost:3000"
+  const currentUrl = baseUrl + router.pathname
+  const defaultImage = `${baseUrl}/images/social-card.png`
+  return (
+    <Helmet>
+      <html lang="ja" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta charSet="utf-8" />
+
+        <title>{title}</title>
+          <meta name="description" content={description} key="description" />
+          <meta name="image" content={defaultImage} key="image" />
+          <link rel="canonical" href={currentUrl} key="canonical" />
+          <meta property="og:title" content={title} key="ogtitle" />
+          <meta property="og:description" content={defaultImage} key="ogdescription" />
+          <meta property="og:image" content={defaultImage} key="ogimage" />
+          <meta property="og:url" content={currentUrl} key="ogurl" />
+      </Helmet>
+  )
+}
+
+export default SEO;
